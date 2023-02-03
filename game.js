ba261484e6ed8434102fde8fc732f87cf19fe64f
Goodnews Adewole <goodnews.adewole@example.com>
2023-02-03 14:19:56 +0100

    changes in my js file

--- game.js
+++ game.js
@@ -3,12 +3,20 @@ const submit = document.getElementById("submit")
 const restart = document.getElementById("restart")
 const inputVal = document.getElementById("input")
 const result = document.getElementById("result")
-
+console.log(randomNumber)
+let missCount = 0
 submit.addEventListener("click", ()=>{
         guessedNumber = inputVal.value
         if(guessedNumber == randomNumber){
-            result.textContent = "hey you guessed the number"
-        }else{result.textContent = "try again"}
+            result.textContent = "Hey you guessed the number. It took you " + missCount + " guesses"
+        }else if(guessedNumber < randomNumber){
+            missCount++
+            result.textContent = "too small"
+        }else if(guessedNumber > randomNumber ){
+            missCount++
+            result.textContent = "too big"
+        }
+        else{result.textContent = "try again"}
 })
 
 restart.addEventListener("click", ()=>{location.reload()})
